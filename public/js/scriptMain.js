626686814e864bba3288c9e99d27edc000921223
cujoproject <jesper@MacBook.local>
2017-03-05 17:14:15 +0100

    Update input form (still very much in progress)

--- public/js/scriptMain.js
+++ public/js/scriptMain.js
@@ -2,11 +2,71 @@
 
 $(document).ready(function(){
   
+  // ToDo: Merge functions
   
-  // Update form unit
-  $( ".target" ).change(function() {
-    alert( "Handler for .change() called." );
-  });
+      // Update form unit
+    $( ".selectActivity" ).change(function() {
+
+      var selVal = $(".selectActivity").val();
+      var output = "";
+
+        switch (selVal) {
+      case "0":
+          output  = "km";
+          break;
+      case "1":
+          output  = "mil";
+          break; 
+      case "2":
+          output  = "km";
+          break; 
+      case "3":
+          output  = "portioner";
+          break; 
+      case "4":
+          output  = "timmar";
+          break; 
+      default: 
+          output = "";
+      }
+
+      $( "#inputUnit").html(output);
+    
+    });
+    
+    
+
+
+    // Update E number
+    $( ".selectNumber" ).change(function() {
+      var selVal = $(".selectNumber").val();
+      var output = "";
+  
+      switch (selVal) {
+      case "0":
+          output  = "22";
+          break;
+      case "1":
+          output  = "12";
+          break; 
+      case "2":
+          output  = "4";
+          break; 
+      case "3":
+          output  = "5";
+          break; 
+      case "4":
+          output  = "8";
+          break; 
+      default: 
+          output = "0";
+      }
+    
+    //$( "#eNumberPresentation").attr('innerHTML', output);        
+    $( "#eNumberPresentation").html(output);        
+          
+  
+    });
   
   
   
