8aa554a52f21164f4e05b8c2fb0cd59cc0109222
cujoproject <kemisten@gmail.com>
2017-08-12 12:35:31 +0200

    js updates generating page

--- public/js/scriptMain.js
+++ public/js/scriptMain.js
@@ -1,215 +1,384 @@
 
-var activityList = { 0:'',1:'cyklade',2:'sprang',3:'gick',4:'åt',5:'sov'};
-var timeList = {0:'Just nu',1:'7 a.m.',2:'9 a.m.',3:'11 a.m.',4:'1 p.m.',5:'3 p.m.',6:'5 p.m.',7:'7 p.m.',8:'9 p.m.',9:'9 p.m.'};
-var eNumbers ={};
-var dataSet;
+
+// ********************
+// data from database
+
+
+
+var titleData = {
+    mainTitle: "Presentation page",
+    paragraphs: [
+        {text:"Who am I?"},
+        {text:"What is this page about?"},
+        {text:"Project background"}
+    ]
+}
+
+
+
+
+
+var cvdata = {
+    titleField:
+    {
+    mainTitle:{
+        header:[
+                {
+            title:"Who is Giuseppe ?",
+            description: "PhD etc"
+            }
+            
+        ]
+        }
+    },
+    
+    infoFields:
+    [
+     {id:"education",
+     description: "Education",
+     content:[
+        {
+            id:"university",
+            description: "University level",
+            content:[
+                {id: "KTH",
+                description: "Master of Science in Chemical Engineering, Royal Inst. of Sthlm",
+                period: "1996-2003"
+                },
+                {id: "phd",
+                description: "PhD in Biophysics, Stockholm University",
+                period: "2005-2009"
+                }                                
+            ]
+            
+        }
+    ]
+     },
+
+     {id:"education",
+     description: "Education",
+     content:[
+        {
+            id:"university",
+            description: "University level",
+            content:[
+                {id: "KTH",
+                description: "Master of Science in Chemical Engineering, Royal Inst. of Sthlm",
+                period: "1996-2003"
+                },
+                {id: "phd",
+                description: "PhD in Biophysics, Stockholm University",
+                period: "2005-2009"
+                }                                
+            ]
+            
+        }
+    ]
+     },
+     
+      {id:"education",
+     description: "Education",
+     content:[
+        {
+            id:"university",
+            description: "University level",
+            content:[
+                {id: "KTH",
+                description: "Master of Science in Chemical Engineering, Royal Inst. of Sthlm",
+                period: "1996-2003"
+                },
+                {id: "phd",
+                description: "PhD in Biophysics, Stockholm University",
+                period: "2005-2009"
+                }                                
+            ]
+            
+        }
+    ]
+     },
+     
+      {id:"education",
+     description: "Education",
+     content:[
+        {
+            id:"university",
+            description: "University level",
+            content:[
+                {id: "KTH",
+                description: "Master of Science in Chemical Engineering, Royal Inst. of Sthlm",
+                period: "1996-2003"
+                },
+                {id: "phd",
+                description: "PhD in Biophysics, Stockholm University",
+                period: "2005-2009"
+                }                                
+            ]
+            
+        }
+    ]
+     },
+      {id:"education",
+     description: "Education",
+     content:[
+        {
+            id:"university",
+            description: "University level",
+            content:[
+                {id: "KTH",
+                description: "Master of Science in Chemical Engineering, Royal Inst. of Sthlm",
+                period: "1996-2003"
+                },
+                {id: "phd",
+                description: "PhD in Biophysics, Stockholm University",
+                period: "2005-2009"
+                }                                
+            ]
+            
+        }
+    ]
+     },
+      {id:"education",
+     description: "Education",
+     content:[
+        {
+            id:"university",
+            description: "University level",
+            content:[
+                {id: "KTH",
+                description: "Master of Science in Chemical Engineering, Royal Inst. of Sthlm",
+                period: "1996-2003"
+                },
+                {id: "phd",
+                description: "PhD in Biophysics, Stockholm University",
+                period: "2005-2009"
+                }                                
+            ]
+            
+        }
+    ]
+     }
+     
+     
+     
+     
+     
+     
+]        
+ 
+};
+
+
+
+
+var infoboxTemplate =`<div class="col-lg-3 col-sm-6">
+            <div class="card hovercard">
+                <div class="cardheader">
+
+                </div>
+                <div class="avatar">
+                    <img alt="" src="../img/profiles/test01.jpeg">
+                </div>
+                <div class="info">
+                    <div class="title">
+                        <a target="_blank" href="http://scripteden.com/">Script Eden</a>
+                    </div>
+                    <div class="desc">Passionate designer</div>
+                    <div class="desc">Curious developer</div>
+                    <div class="desc">Tech geek</div>
+                </div>
+                <div class="bottom">
+                    <a class="btn btn-primary btn-twitter btn-sm" href="https://twitter.com/webmaniac">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                    <a class="btn btn-danger btn-sm" rel="publisher"
+                       href="https://plus.google.com/+ahmshahnuralam">
+                        <i class="fa fa-google-plus"></i>
+                    </a>
+                    <a class="btn btn-primary btn-sm" rel="publisher"
+                       href="https://plus.google.com/shahnuralam">
+                        <i class="fa fa-facebook"></i>
+                    </a>
+                    <a class="btn btn-warning btn-sm" rel="publisher" href="https://plus.google.com/shahnuralam">
+                        <i class="fa fa-behance"></i>
+                    </a>
+                </div>
+            </div>
+
+        </div>`
+
+
+
+
+
+
+
 
 $(document).ready(function(){
 
-    
-    getData(function(dataSet){
-        calculateEnumberMain(dataSet);
-        updateEventList();
-            });
 
-                        
-     function calculateEnumberMain(dataSet){
-                
-        for (var key in activityList) {
-            var tempValueArray = extractValues(dataSet, key);
-            var curEnum = calcEnum(tempValueArray);
-            eNumbers[key] = curEnum;
-            updateEnumber();
+    //$( "#eNumberPresentation").html(output);
+
+    
+    //function addInfoDiv(curDiv, curId, title){
+    //    $( "#" + curDiv )
+    //    .prepend('<div id="' + curId + '" class="col-xs-6 col-lg-4"></div>')
+    //    
+    //    $( "#" + curId )
+    //    .prepend('<h2>' + title +'</h2>')
+    //    .prepend('<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>');        
+    //    return;        
+    //    }    
+    
+    
+    function updateInfoDiv(curDiv, curId, title, description){
+        $( "#" + curDiv )
+        .prepend('<div id="' + curId + '" class="subInfoDiv"></div>')
+        
+        $( "#" + curId )
+        .prepend('<h2>' + title +'</h2>')
+        .prepend('<p>'+description+'</p>');        
+        return;        
         }
-        return;    
-    } 
-
-
-    function calcEnum(curArray){
         
-        var maxLen = curArray.length;
-        for (var m = curArray.length; m > 0; m--){
-            if (curArray[maxLen - m] >= m){
-                return m;                
-            }
-        //return '?';
-        }
-        return '?';
-    
-    }
-    
+    //function addLevel(infoObj, parent){
+    //    for (var objIndex = 0; objIndex < infoObj.length; objIndex++) {
+    //        console.log("infoObj[objIndex].id: ", infoObj[objIndex].id)
+    //        addInfoDiv(parent, infoObj[objIndex].id, infoObj[objIndex].description);            
+    //    }
+    //    return infoObj[objIndex].id;
+    //    }       
 
 
-    function extractValues(dataSet, i)
-    {
-        var valueArray = [];
-        dataSet.forEach(function(item) {
-            item = JSON.parse(item);
-            if (item.activityCode == i){
-                valueArray.push(item.dataValue);    
-            }
-        });
-        return valueArray;
-    }
-    
-  
-  	function clearForm() {
-        $( "#selectActivity").val('0');
-        $( "#inputValue").val("");       
-        $( "#timePoint").val('0');
-	}
-  
-    function getData(callback) {
-  		$.post('/getDataAws',
-		{
-            'data':'test'
-		},
-		function(resp){
-            dataSet = resp.data;
-            callback(resp.data);
-		});	
-    }
-  
-  
-    
-  $( "#clearForm" ).click(function() {      	    
-    clearForm();
-  });
-  
-  
-  
-  $( "#sendPost" ).click(function() {
-    
-    if ($( "#selectActivity").val() === '0' || $( "#inputValue").val() === '') {
-        alert("Var snäll och fyll i alla värden");
-        return;
-    }
-    
-    var now = new Date();
-    
-    var sendPostObject = {
-        'activityCode': $( "#selectActivity").val(),
-        'activityDescription': activityList[$( "#selectActivity").val()],
-       
-        'dataValue': $( "#inputValue").val(),
-        'dataUnit': $( "#inputUnit").text(),        
-        
-        'timepointCode': $( "#timePoint").val(),
-        'timepointDescription': timeList[$( "#timePoint").val()],
-        'timestamp' : now
-        
-    };
-    console.log("sendPostObject: ", sendPostObject)
-    sendPost(sendPostObject,
-             function() {
-                getData(
-                        function(output){
-                            //console.log("==========\n Data fetched: ", output)
-                            //updateEnumber();
-                            updateEventList();
-                            calculateEnumberMain(dataSet);
 
-                        });
-                clearForm();
-             });
-  });
-  
-  
-    
-  	function sendPost(arg1, callback) {
-		$.post('/addDataAws',
-		{
-            'data':arg1
-		},
-		function(curUserInfo){
-			callback(curUserInfo);
-		});	
-
-	}
-  
-  
-    function updateEventList(){
-        var tmpString = "";
-        dataSet.forEach(function(item) {
-            item = JSON.parse(item);            
-            tmpString += "<p> Jag " + item.activityDescription + " " + item.dataValue + " " + item.dataUnit + " klockan " + item.timepointDescription + ", lagrat " + item.timestamp + "</p>";
-            //console.log(tmpString);
-                    
-        });
-        $( "#dataCollection").html(tmpString);            
-        
-    }
-  
-  
-      // Update form unit
-    $( ".selectActivity" ).change(function() {
-
-      var selVal = $(".selectActivity").val();
-      var output = "";
-
-        switch (selVal) {
-      case "0":
-          output  = "";
-          break;
-      case "1":
-          output  = "mil";
-          break;
-      case "2":
-          output  = "km";
-          break; 
-      case "3":
-          output  = "km";
-          break; 
-      case "4":
-          output  = "portioner";
-          break; 
-      case "5":
-          output  = "timmar";
-          break; 
-      default: 
-          output = "";
-      }
-
-      $( "#inputUnit").html(output);
-    
-    });
-    
-  function updateEnumber() {
-    
-    var selVal = $(".selectNumber").val();
-      var output = "";
-  
-      switch (selVal) {
-      case "0":
-          output  = eNumbers[0];
-          break;
-      case "1":
-          output  = eNumbers[1];
-          break; 
-      case "2":
-          output  = eNumbers[2];
-          break; 
-      case "3":
-          output  = eNumbers[3];
-          break; 
-      case "4":
-          output  = eNumbers[4];
-          break; 
-      default: 
-          output = eNumbers[5];
-      }
-    
-    $( "#eNumberPresentation").html(output);
+    function addInfoDiv(curDiv, curId, title){
+        $( "#" + curDiv )
+        .prepend(infoboxTemplate)
             
-  }
+            
+            
+        //$( "#" + curDiv )
+        //.prepend('<div id="' + curId + '" class="col-xs-6 col-lg-4"></div>')        
+        //$( "#" + curId )
+        //.prepend('<h2>' + title +'</h2>')
+        //.prepend('<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>');        
+
+
+
+        return;        
+        } 
+
+
+
+    function addLevel(infoObj, parent){
+        for (var objIndex = 0; objIndex < infoObj.length; objIndex++) {
+            console.log("infoObj[objIndex].id: ", infoObj[objIndex].id);
+            addInfoDiv(parent, infoObj[objIndex].id, infoObj[objIndex].description);            
+        }
+        return infoObj[objIndex].id;
+        }       
+
+        
+
+    function updateInfoBoxes(cvdata) {
+        
+        var mainParent = "infoContainer";
+        var infoObj = cvdata.infoFields;
+        
+        
+        
+        var child = addLevel(infoObj, mainParent);
+            
+
+
+
+
+            
   
-  
-  
-    // Update E number
-    $( ".selectNumber" ).change(function() {
-        updateEnumber();
-    });
+        
+        
+        //var infoObj = cvdata.infoFields;
+        //
+        //for (var objIndex = 0; objIndex < infoObj.length; objIndex++) {
+        //    console.log("Cur Object: ", infoObj[objIndex].id);            
+        //    addInfoDiv("infoContainer", infoObj[objIndex].id, infoObj[objIndex].description);
+        //                
+        //    
+        //    var subInfoObj = infoObj[objIndex].content;
+        //    for (var subObjIndex = 0; subObjIndex < subInfoObj.length; subObjIndex++) {
+        //        updateInfoDiv(infoObj[objIndex].id, subInfoObj[subObjIndex].id, subInfoObj.id, subInfoObj[subObjIndex].description);
+        //        
+        //        
+        //    }
+            
+            
+
+            
+        }
+
+
     
+    //function updateInfoBoxes() {
+    //    
+    //    var infoObj = cvdata.infoFields;
+    //    
+    //    for (var objIndex = 0; objIndex < infoObj.length; objIndex++) {
+    //        console.log("Cur Object: ", infoObj[objIndex].id);            
+    //        addInfoDiv("infoContainer", infoObj[objIndex].id, infoObj[objIndex].description);
+    //                    
+    //        
+    //        var subInfoObj = infoObj[objIndex].content;
+    //        for (var subObjIndex = 0; subObjIndex < subInfoObj.length; subObjIndex++) {
+    //            updateInfoDiv(infoObj[objIndex].id, subInfoObj[subObjIndex].id, subInfoObj.id, subInfoObj[subObjIndex].description);
+    //            
+    //            
+    //        }
+    //        
+    //        
+    //
+    //        
+    //    }
+        
+        
+        
+         //for (var key in infoObj){
+         //   console.log("Key: ", key);
+         //   addDiv("infoContainer", key, key)
+         //   for (var key2 in infoObj[key]){
+         //       console.log("Key2: ", key2);
+         //       for (var item in infoObj[key][key2]){
+         //           console.log("Item: ", cvdata.infoFields[key][key2][item]);
+         //       }
+         //       
+         //   
+         //   }
+         //   
+         //}
+        
+        
+        //$.each(curList, function(seq, obj) {
+        //    var selFlag = false;
+        //    if (obj.id === selectedId) {selFlag = true};        
+        //    $(curElement).append(new Option(obj.description, obj.id, selFlag, selFlag));                
+        //});
+	
+    //}
+    
+    
+    function updateJumbo(titleData){
+    
+    $( "#jumbotron").append('<h1>'+titleData.mainTitle+'</h1>');
+
+    for (var objIndex = 0; objIndex < titleData.paragraphs.length; objIndex++) {
+        $( "#jumbotron").append('<p>'+titleData.paragraphs[objIndex].text+'</p>');        
+    }
+
+    return;
+    }  
+
+    
+    
+    updateJumbo(titleData);
+    updateInfoBoxes(cvdata);
   });
 
 
